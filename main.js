1a89b5b2ad29c7eaad455c083c4ee7801ca46ef4
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-15 19:51:47 +0530

    accordion styles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*creating mobile navbar*/
 
-var togglebutton = document.querySelector(".togglebutton");
+    var togglebutton = document.querySelector(".togglebutton");
     var nav = document.querySelector(".navlinks");
     var links = document.querySelector(".navlinks li");
 
@@ -8,3 +8,14 @@ var togglebutton = document.querySelector(".togglebutton");
     this.classList.toggle("click");
     nav.classList.toggle("open");
     })
+
+
+/* accordion*/
+const accordion = document.getElementsByClassName
+('contentBox');
+
+for(i=0; i<accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+    })
+}
